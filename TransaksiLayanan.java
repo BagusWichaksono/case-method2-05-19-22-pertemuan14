21c49bd5929bf180e16caecd4b5709ca3502041c
Zuhdiaja <145771767+Zuhdiaja@users.noreply.github.com>
2025-06-02 08:34:08 +0700

    transaksi layanan

--- TransaksiLayanan.java
+++ TransaksiLayanan.java
@@ -1,3 +1,20 @@
 public class TransaksiLayanan {
-    
+    Pasien pasien;
+    Dokter dokter;
+    int durasiLayanan;
+    int biaya;
+
+    public TransaksiLayanan(Pasien pasien, Dokter dokter, int durasi){
+        this.pasien = pasien;
+        this.dokter = dokter;
+        this.durasiLayanan = durasi;
+    }
+
+    public int hitungBiaya(){
+        return durasiLayanan * 50000;
+    }
+
+    public void tampilkantransaksi(){
+        System.out.println(pasien.nama + " " + durasiLayanan + " jam : Rp " + biaya);
+    }
 }
